e9b9e6af06df17553ea6c92f36ce7b2910c49922
= <=>
2024-05-30 11:26:07 -0300

    Término 11

--- livraria/models.py
+++ livraria/models.py
@@ -37,3 +37,4 @@ class Livro(models.Model):
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros")
     def __str__(self):
         return f"{self.titulo} ({self.quantidade})"
+    autores = models.ManyToManyField(Autor, related_name="livros")

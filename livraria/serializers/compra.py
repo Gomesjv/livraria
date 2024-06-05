edeeca6fd66ccb75c45aaf5e374f15a88bac5436
= <=>
2024-06-05 10:56:33 -0300

    Término da 28

--- livraria/serializers/compra.py
+++ livraria/serializers/compra.py
@@ -1,10 +1,20 @@
-from rest_framework.serializers import ModelSerializer, CharField
+from rest_framework.serializers import ModelSerializer, CharField, SerializerMethodField
 
-from livraria.models import Compra
+from livraria.models import Compra, ItensCompra
+
+class ItensCompraSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = "__all__"
+        depth = 1
+total = SerializerMethodField()
+def get_total(self, instance):
+    return instance.quantidade * instance.livro.preco
 
 class CompraSerializer(ModelSerializer):
     class Meta:
         model = Compra
-        fields = "__all__"
+        fields = 2
 usuario = CharField(source="usuario.email", read_only=True)
-
+status = CharField(source="get_status_display", read_only=True)
+itens = ItensCompraSerializer(many=True, read_only=True)

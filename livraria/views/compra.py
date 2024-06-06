fa9cef05372a0043cc3a99381d232fed95b7d294
= <=>
2024-06-06 10:29:04 -0300

    Término 34

--- livraria/views/compra.py
+++ livraria/views/compra.py
@@ -5,9 +5,11 @@ from livraria.serializers import CompraSerializer, CriarEditarCompraSerializer
 
 class CompraViewSet(ModelViewSet):
     queryset = Compra.objects.all()
-    serializer_class = CompraSerializer
 
-    def get_serializer_class(self):
-        if self.action == "create" or self.action == "update":
-            return CriarEditarCompraSerializer
-        return CompraSerializer
+    def get_queryset(self):
+        usuario = self.request.user
+        if usuario.is_superuser:
+            return Compra.objects.all()
+        if usuario.groups.filter(name="Administradores"):
+            return Compra.objects.all()
+        return Compra.objects.filter(usuario=usuario)

02ba1e8f9b545d7d0464694e822b0810f2a20457
= <=>
2024-06-06 10:20:18 -0300

    Término 30

--- livraria/serializers/compra.py
+++ livraria/serializers/compra.py
@@ -18,3 +18,22 @@ class CompraSerializer(ModelSerializer):
 usuario = CharField(source="usuario.email", read_only=True)
 status = CharField(source="get_status_display", read_only=True)
 itens = ItensCompraSerializer(many=True, read_only=True)
+
+class CriarEditarItensCompraSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = ("livro", "quantidade")
+
+class CriarEditarCompraSerializer(ModelSerializer):
+    itens = CriarEditarItensCompraSerializer(many=True)
+    class Meta:
+        model = Compra
+        fields = ("usuario", "itens")
+
+    def create(self, validated_data):
+        itens_data = validated_data.pop("itens")
+        compra = Compra.objects.create(**validated_data)
+        for item_data in itens_data:
+            ItensCompra.objects.create(compra=compra, **item_data)
+        compra.save()
+        return compra
--- livraria/views/compra.py
+++ livraria/views/compra.py
@@ -1,9 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
 
 from livraria.models import Compra
-from livraria.serializers import CompraSerializer
-
+from livraria.serializers import CompraSerializer, CriarEditarCompraSerializer
 
 class CompraViewSet(ModelViewSet):
     queryset = Compra.objects.all()
     serializer_class = CompraSerializer
+
+    def get_serializer_class(self):
+        if self.action == "create" or self.action == "update":
+            return CriarEditarCompraSerializer
+        return CompraSerializer

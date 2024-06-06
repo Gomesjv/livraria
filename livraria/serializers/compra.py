824d8ee1598d5050ce329abdb8a2ff02200bcda0
= <=>
2024-06-06 10:22:24 -0300

    Término 31

--- livraria/serializers/compra.py
+++ livraria/serializers/compra.py
@@ -18,6 +18,14 @@ class CompraSerializer(ModelSerializer):
 usuario = CharField(source="usuario.email", read_only=True)
 status = CharField(source="get_status_display", read_only=True)
 itens = ItensCompraSerializer(many=True, read_only=True)
+def update(self, instance, validated_data):
+        itens = validated_data.pop("itens")
+        if itens:
+            instance.itens.all().delete()
+            for item in itens:
+                ItensCompra.objects.create(compra=instance, **item)
+        instance.save()
+        return instance
 
 class CriarEditarItensCompraSerializer(ModelSerializer):
     class Meta:

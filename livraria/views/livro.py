9f2d408fc90dc61883919426c6a7fa8b1e16b164
= <=>
2024-06-07 16:19:11 -0300

    Término 37

--- livraria/views/livro.py
+++ livraria/views/livro.py
@@ -2,13 +2,15 @@ from rest_framework.viewsets import ModelViewSet
 from livraria.models import Livro
 from livraria.serializers import LivroSerializer, LivroDetailSerializer, LivroListSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = filterset_fields = ["categoria__descricao", "editora__nome"]
-    
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ["categoria__descricao", "editora__nome"]
+    search_fields = ["titulo"]
+
     def get_serializer_class(self):
         if self.action == "list":
             return LivroListSerializer
